b6968a415ba12324070fc33f0bdb951883d5188b
SamSKS <samuel.meireles.15s@outlook.com>
2022-11-27 00:16:09 -0300

    add: class DoorModel

--- src/models/door.js
+++ src/models/door.js
@@ -1,3 +1,44 @@
-export default class Door {
+export default class DoorModel {
   #number;
+  #withGift;
+  #selected;
+  #opened;
+
+  constructor(number, withGift = false, selected = false, opened = false) {
+    this.#number = number;
+    this.#withGift = withGift;
+    this.#selected = selected;
+    this.#opened = opened;
+  }
+
+  get number() {
+    return this.#number;
+  }
+
+  get withGift() {
+    return this.#withGift;
+  }
+
+  get selected() {
+    return this.#selected;
+  }
+
+  get opened() {
+    return this.#opened;
+  }
+
+  unselectDoor() {
+    const selected = false;
+    return new Door(this.number, this.withGift, selected, this.opened);
+  }
+
+  selectDoor() {
+    const selected = !this.selected;
+    return new Door(this.number, this.withGift, selected, this.opened);
+  }
+
+  openDoor() {
+    const opened = true;
+    return new Door(this.number, this.withGift, this.selected, opened);
+  }
 }
